2b08acde7f119cdeed67fa1198591e7de903e202
arup-sarkar-24 <1arupsarkar112@gmail.com>
2024-05-29 22:26:49 +0530

    2nd commit

--- src/main/java/com/mycompany/contacthub/controllers/PageController.java
+++ src/main/java/com/mycompany/contacthub/controllers/PageController.java
@@ -10,9 +10,30 @@ public class PageController {
     @RequestMapping("/home")
     public String home(Model model){
         System.out.println("home page handler");
-        model.addAttribute("name","Substring names");
+        model.addAttribute("pageName","home");
         model.addAttribute("address","localhost");
         model.addAttribute("url","8081");
         return "home";
+    }
+
+    @RequestMapping("/about")
+    public String about(Model model){
+        System.out.println("about page handler");
+        model.addAttribute("pageName","About");
+        return "about";
+    }
+
+    @RequestMapping("/services")
+    public String services(Model model){
+        System.out.println("service page handler");
+        model.addAttribute("pageName","Service");
+        return "services";
+    }
+
+    @RequestMapping("/contact")
+    public String contact(Model model){
+        System.out.println("contact page handler");
+        model.addAttribute("pageName","Contact");
+        return "contact";
     }
 }
